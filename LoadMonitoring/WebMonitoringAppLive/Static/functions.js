301526dcf16cc77c80b1311aae4536106d49a354
Sergei Puzyrnyi <serg.p94@gmail.com>
2015-06-02 01:42:24 +0300

    Widget and update added

--- LoadMonitoring/WebMonitoringAppLive/Static/functions.js
+++ LoadMonitoring/WebMonitoringAppLive/Static/functions.js
@@ -1,24 +1,24 @@
 ﻿function Clear(dest) {
-    var n = $(dest).children("div").length;
-    RemoveCol(dest, n);
+    dest.children("div").remove();
 }
 
 function AddCol(dst, h, w) {
     var col = $(document.createElement('div'));
-    var areaHeight = 300;
     col.addClass('paint-col');
     col.addClass('text-center');
     col.css('width', '0');
     col.css('height', h);
-    col.css('margin-top', areaHeight - h - 2);
+    col.css('margin-top', painter.maxHeight - h - 2);
     col.animate({ "width": w + "%" });
     $(dst).append(col);
     return col;
 }
 
-function RemoveCol(dst, n) {
-    var cols = $(dst).find('.paint-col').slice(0, n);
-    cols.animate({ width: 0 }, function () {
-        cols.remove();
+function RemoveCol(dst) {
+    var col = $(dst).find('.paint-col').first();
+    col.animate({ width: 0 }, {
+        complete: function () {
+            col.remove();
+        }
     });
 }
