87a31c0be083a5692f79c84b22e6a5087593d967
Sergei Puzyrnyi <serg.p94@gmail.com>
2015-07-08 14:59:11 +0300

    WebUI added

---
+++ HomeAssistant/UI.WebApp/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using BL.Users;
+using System.Web.Mvc;
+
+namespace UI.WebApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Register(string login, string password)
+        {
+            if (login == null && password == null)
+            {
+                return View();
+            }
+            else
+            {
+                var um = Bootstrapper.Bootstrapper.GetUserManager();
+                return RedirectToAction("RegistrationResult", new { result = um.Register(new User { Login = login, Password = password }) });
+            }
+        }
+
+        public ActionResult RegistrationResult(UserRegistrationResult result)
+        {
+            return View(result);
+        }
+    }
+}
